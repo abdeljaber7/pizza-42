61ecb50dd2492694b0d54564626f064244769ed1
Mousa Abdeljaber <mousa.abdeljaber@gmail.com>
2021-03-21 23:13:19 +0400

    make it better

---
+++ README.md
@@ -0,0 +1,21 @@
+# Pizza 42
+
+a simple App to use APIs for Pizza ordering
+
+## Running the Application
+
+Run the application locally:
+
+ - npm install
+ - npm start
+
+Open the application in the browser at [http://localhost:3000](http://localhost:3000).
+
+## Author
+
+Mousa Abdeljaber
+[Auth0](auth0.com)
+
+## License
+
+This project is licensed under the MIT license. See the [LICENSE](LICENSE.txt) file for more info.
---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const { join } = require("path");
+const authConfig = require("./auth_config.json");
+
+const app = express();
+
+// Orders DB
+let orders =[];
+let orderTime =[];
+
+
+if (!authConfig.domain || !authConfig.audience) {
+  throw "Please make sure that auth_config.json is in place and populated";
+}
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(express.static(join(__dirname, "public")));
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithms: ["RS256"]
+});
+
+//New Order API
+app.get(`/orders/new-order`, checkJwt, (req, res) => {
+  const email = req.query.email;
+  //log Orders
+  orders.push(
+    email
+    );
+  orderTime.push(
+    new Date()
+  );
+  res.send({
+    msg: `success`
+  });
+});
+
+//Orders History API
+app.get(`/orders/order-history`, checkJwt, (req, res) => {
+  const email = req.query.email;
+  let history =[];
+  for(let i=0; i<orders.length; i++){
+    if(orders[i]==email){
+      history.push(orderTime[i])
+    }
+  }
+  res.send({
+    msg: `${history}`
+  });
+});
+
+app.get("/auth_config.json", (req, res) => {
+  res.sendFile(join(__dirname, "auth_config.json"));
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+app.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).send({ msg: "Invalid token" });
+  }
+
+  next(err, req, res);
+});
+
+process.on("SIGINT", function() {
+  process.exit();
+});
+
+// Listen on port 3000
+app.listen(3000, () => console.log("Pizza 42 is up!\n Access application on http://localhost:3000"));
